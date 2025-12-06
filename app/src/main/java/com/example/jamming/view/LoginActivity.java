32fa2aea2dd263fe1dbd01d160439cb210ee4d1e
shirat <shirat345@gmail.com>
2025-12-06 21:39:12 +0200

    create new package

--- app/src/main/java/com/example/jamming/view/LoginActivity.java
+++ app/src/main/java/com/example/jamming/view/LoginActivity.java
@@ -1,13 +1,11 @@
-package com.example.jamming;
+package com.example.jamming.view;
 
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.jamming.R;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/jamming/view/profilePreferencesActivity.java
+++ app/src/main/java/com/example/jamming/view/profilePreferencesActivity.java
@@ -1,4 +1,4 @@
-package com.example.jamming;
+package com.example.jamming.view;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -9,6 +9,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.jamming.R;
+
 public class profilePreferencesActivity extends AppCompatActivity {
 
     @SuppressLint("MissingInflatedId")
@@ -17,10 +19,6 @@ public class profilePreferencesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile_preferences);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
