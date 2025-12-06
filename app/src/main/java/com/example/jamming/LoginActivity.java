4401e2224ee7562dbbe948db22e51cc4094e60cd
shirat <shirat345@gmail.com>
2025-12-06 21:11:42 +0200

    Add Profile & Preferences User screen.2

--- app/src/main/java/com/example/jamming/LoginActivity.java
+++ app/src/main/java/com/example/jamming/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.jamming;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button loginButton = findViewById(R.id.loginButton);
+
     }
 }
