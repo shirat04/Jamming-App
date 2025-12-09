a714454d8d9076507b3d7f5cd9034a2a0809681c
shirat <shirat345@gmail.com>
2025-12-09 15:07:44 +0200

    Edit Event screen

---
+++ app/src/main/java/com/example/jamming/view/EditEventActivity.java
@@ -0,0 +1,21 @@
+package com.example.jamming.view;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.jamming.R;
+
+public class EditEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+    }
+}
