bc1a87a2783398fd339f772ba3be61705cd7b776
Or-Deri <orderi429@gmail.com>
2025-12-11 11:02:05 +0200

    חיבור המסך של יצירת אירוע

--- app/src/main/java/com/example/jamming/view/OwnerActivity.java
+++ app/src/main/java/com/example/jamming/view/OwnerActivity.java
@@ -1,6 +1,8 @@
 package com.example.jamming.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.jamming.R;
 
 public class OwnerActivity extends AppCompatActivity {
+    private Button createEventButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_owner);
 
+        createEventButton = findViewById(R.id.createEventButton);
+        createEventButton.setOnClickListener(v -> {
+            Intent intent = new Intent(OwnerActivity.this, CreateNewEvent.class);
+            startActivity(intent);
+        });
+
+
     }
+
 }
